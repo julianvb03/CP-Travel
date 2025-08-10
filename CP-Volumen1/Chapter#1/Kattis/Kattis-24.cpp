0de0849d710688ee09a69e17b0c4666f37f23135
Julian Valencia <valenciajuliann@hotmail.com>
2025-08-10 15:43:38 -0500

    si

---
+++ CP-Volumen1/Chapter#1/Kattis/Kattis-24.cpp
@@ -0,0 +1,12 @@
+//bossbattle
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n;
+    scanf("%d", &n);
+    if(n <= 3)
+        printf("1\n");
+    else
+        printf("%d\n", n-2);
+}
